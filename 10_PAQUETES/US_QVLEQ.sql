25eb835675acc549795c835454bcb3d7d3b72531
TheOverLordKotan <dajo@ue.edu.co>
2018-05-01 17:56:30 -0500

    arreglar los paquetes

--- 10_PAQUETES/US_QVLEQ.sql
+++ 10_PAQUETES/US_QVLEQ.sql
@@ -31,17 +31,16 @@ create or replace PACKAGE BODY FS_ADDEP_US.US_QVLEQ AS
 		v_Id_Rol number;
 	
 		BEGIN
-		
 			SELECT COUNT (*) INTO v_Usuario_Unico FROM US_DTEQ WHERE DTEQ_USRI = p_Id_Usuario;
-			SELECT USRS_ROLS INTO v_Id_Rol FROM US_USRS WHERE USRS_USRI = v_Id_Usuario;	
-			
+			SELECT USRS_ROLS INTO v_Id_Rol FROM US_USRS WHERE USRS_USRI = p_Id_Usuario;	
+
 			IF(v_Usuario_Unico = 0 AND v_Id_Rol = 2)THEN
-			
+
 				cod_respuesta:='OK';
 			ELSE
 
 				cod_respuesta:='ERROR';
 				msg_respuesta:='SU USUARIO YA TIENE UN EQUIPO REGISTRADO O NO TIENE PERMISO';
 			END IF;
-		
+    END VALIDACION_EQUIPO;
 END US_QVLEQ;		
